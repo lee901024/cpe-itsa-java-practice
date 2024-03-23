5d56366b6ff61fd360d408fa6debeee6f8a7dcd1
leemikepop <leemikepop@gmail.com>
2024-03-24 00:50:45 +0800

    uva11576

---
+++ CPE20231017/001_uva11576/main.cpp
@@ -0,0 +1,21 @@
+//Uva 11576
+/*
+https://cpe.cse.nsysu.edu.tw/cpe/file/attendance/problemPdf/11576.pdf
+*/
+
+#include <iostream>
+#include <sstream>
+//#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    clock_t start = clock();
+    ostringstream oss;
+    
+    cout << oss.str();
+    clock_t end = clock();
+    double duration = (double)(end - start) / (__clock_t)1000;
+    cout << endl << "Exicution Time: " << duration << " ms" << endl;
+    return 0;
+}
